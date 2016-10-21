be625671a4904b8fd118227edb51b4c709235d5d
Neil Burchfield <neil@nativ.com>
2016-10-20 22:31:30 -0500

    first commit

---
+++ HCButton/HCButton.h
@@ -0,0 +1,13 @@
+//
+//  HCButton.h
+//  HCButton
+//
+//  Created by Neil Burchfield on 10/20/16.
+//  Copyright © 2016 Neil Burchfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCButton : UIButton
+
+@end
---
+++ HCButton/HCButton.m
@@ -0,0 +1,21 @@
+//
+//  HCButton.m
+//  HCButton
+//
+//  Created by Neil Burchfield on 10/20/16.
+//  Copyright © 2016 Neil Burchfield. All rights reserved.
+//
+
+#import "HCButton.h"
+
+@implementation HCButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
